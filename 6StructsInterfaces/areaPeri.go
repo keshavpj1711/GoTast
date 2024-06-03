29a0037dbf6fd4d209fcf1f223f8390aa290b730
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-04 02:15:31 +0530

    More test added to know more about struct

--- 6StructsInterfaces/areaPeri.go
+++ 6StructsInterfaces/areaPeri.go
@@ -1,10 +1,22 @@
 package main
 
+import "math"
+
+const Pi float64 = 3.14
+
 type Rectangle struct{
 	Length float64
 	Breadth float64
 }
 
+type Circle struct{
+	Radius float64
+}
+
 func PeriRect(rect Rectangle) float64 {
 	return 2*(rect.Length + rect.Breadth)
+}
+
+func AreaCircle(circle Circle) float64 {
+	return Pi*(math.Pow(circle.Radius, 2))
 }
--- 6StructsInterfaces/areaPeri_test.go
+++ 6StructsInterfaces/areaPeri_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestPeriRect(t *testing.T)  {
-	got := PeriRect(Rectangle{19, 2})
-	want := 42.0
+	t.Run("To calculate peri of rectangle", func(t *testing.T) {
+		got := PeriRect(Rectangle{19, 2})
+		want := 42.0
 
-	if got != want {
-		t.Errorf("Got %v want %v", got, want)
-	}
+		if got != want {
+			t.Errorf("Got %v want %v", got, want)
+		}
+	})
+
+	t.Run("To calculate area of circle", func(t *testing.T) {
+		got := AreaCircle(Circle{10})
+		want := 314.0
+
+		if got != want {
+			t.Errorf("Got %v want %v", got, want)
+		}
+	})
 }
