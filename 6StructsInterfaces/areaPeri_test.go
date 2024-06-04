afb99893df7851156c818614538e4d78db4a3046
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-04 20:52:38 +0530

    added about table driven tests and changed my code accordingly

--- 6StructsInterfaces/areaPeri_test.go
+++ 6StructsInterfaces/areaPeri_test.go
@@ -7,22 +7,41 @@ func TestPeriRect(t *testing.T) {
 	// But i will be using it this time
 
 	// This is another form of defining a function
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
+
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12, 6}
+	// 	checkArea(t, rectangle, 72.0)
+	// })
+
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314)
+	// })
+
+	// All this above test can be simplified to 
+	// To the following lines
+
+	// This is a form of defining anonymous structs as suggested by the name it can't be used everywhere
+	areaTests := []struct {
+		shape Shape
+		want float64
+	}{
+		{Rectangle{12, 3}, 36.0},
+		{Circle{10}, 314},
+	}
+	// After defining the struct we put two values in it in following way
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+
+		if got != tt.want {
+			t.Errorf("Got %v want %v", got, tt.want)
 		}
 	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314)
-	})
-
 }
