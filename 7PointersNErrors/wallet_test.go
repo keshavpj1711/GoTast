2f245d77080e62b7023ef92242d70a3aa5d8d508
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-07 01:29:27 +0530

    making our code more readable

--- 7PointersNErrors/wallet_test.go
+++ 7PointersNErrors/wallet_test.go
@@ -3,33 +3,7 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-
-	check := func (t testing.TB, wallet Wallet, want Bitcoin)  {
-		t.Helper()
-
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	checkError := func (t testing.TB, got error, want string)  {
-		t.Helper()
-
-		if got == nil {
-			// This .Fatal will stop the test if it's called
-			// This is because we don't want to make any more assertions on the error returned if there isn't one around. 
-			// Without this the test would carry on to the next step and panic because of a nil pointer.
-			t.Fatal("Didn't get an error but wanted one")
-		}
-
-		// This .Error() helps us to convert errors into a string
-		if got.Error() != want {
-			t.Errorf("got %v want %v", got, want)
-		}
-	}
-
+	
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -54,4 +28,30 @@ func TestWallet(t *testing.T) {
 		checkError(t, err, ErrInsufficientFunds.Error())
 		check(t, wallet, startingBalance)
 	})
+}
+
+func check (t testing.TB, wallet Wallet, want Bitcoin)  {
+	t.Helper()
+
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func checkError(t testing.TB, got error, want string)  {
+	t.Helper()
+
+	if got == nil {
+		// This .Fatal will stop the test if it's called
+		// This is because we don't want to make any more assertions on the error returned if there isn't one around. 
+		// Without this the test would carry on to the next step and panic because of a nil pointer.
+		t.Fatal("Didn't get an error but wanted one")
+	}
+
+	// This .Error() helps us to convert errors into a string
+	if got.Error() != want {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
