34c8791647bba769f3c546c11b599ba1fb7a3512
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-16 14:01:57 +0530

    getting to know about maps

---
+++ 8Maps/dict_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+
+	// Defining a map(dictionary) in GO
+	dictionary := map[string]string{"test": "this is a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is a test"
+
+	if got != want {
+		t.Errorf("got %q want %q given %q", got, want, "test")
+	}
+}
