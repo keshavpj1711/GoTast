97fb3b19b538b99349f55d58a346b047a0dda6ec
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-27 19:34:30 +0530

    explaining io.Writer for testing and real world application

--- 9DependencyInjection/greet.go
+++ 9DependencyInjection/greet.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"bytes"
+	// "bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string)  {
+// Instead of *bytes.Buffer we use io.Writer type
+// Reason:
+// *bytes.Buffer implements io.Writer and Greet does not specify what kind of Writer it wants 
+// basically any type which implement io.Writer works
+// So In the testing scenario,
+// you use a bytes.Buffer because it allows you to capture the output from Greet and verify its content.
+// In the real application,
+// you use os.Stdout (standard output) as the writer for Greet.
+
+func Greet(writer io.Writer, name string)  {
 
   // This is what it looks like inside a Printf()
   fmt.Fprintf(writer, "Hello %s", name)
   
   // One thing to note is that this line stdout even if our test fail 
   // fmt.Printf("Hello %s", name)
+}
+
+func main()  {
+  Greet(os.Stdout, "Keshav")
 }
