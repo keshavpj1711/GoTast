b06a9eb1aef49a1da0d6873be0e8f3f82de4ed26
Keshav Prajapati <keshavpj1711@gmail.com>
2024-06-30 02:57:21 +0530

    added into main function

--- 10Mocking/countdown.go
+++ 10Mocking/countdown.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -13,6 +14,7 @@ func Countdown(out io.Writer) {
 
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1*time.Second)
 	}
 
 	fmt.Fprint(out, finalWord)
