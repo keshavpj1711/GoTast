5432c68249a7e2f262bbc61ea4617a1c4aac20ef
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-01 13:57:49 +0530

    created mock sleeper to be run in tests

--- 10Mocking/countdown.go
+++ 10Mocking/countdown.go
@@ -10,16 +10,41 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+// Let's define our dependency as an interface.
+// This lets us then use a real Sleeper in main and a spy sleeper in our tests.
+type Sleeper interface {
+	Sleep()
+}
+
+// So we created two Sleeper one to be called in main function that works as normal Sleeper
+// Other one to be used in tests as it's just calling sleep with out any time passing and increasing call counts
+type SpySleeper struct {
+	Calls int
+}
+type DefaultSleeper struct{}
+
+// A function to implement default sleeping when called in main
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+
+}
+
+// A function to call spySleeper when used in tests and everytime sleep is called it inc calls
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1*time.Second)
+		sleeper.Sleep()
 	}
 
 	fmt.Fprint(out, finalWord)
 }
 
-func main()  {
-	Countdown(os.Stdout)
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
